e0ce6ab005e0d23bf76ad310af351ff0739149c8
Joseph Greef <joseph.k.greef@gmail.com>
2020-08-21 22:58:35 -0700

    Remove commented code from Initializer

--- initializer.h
+++ initializer.h
@@ -8,24 +8,8 @@
 
 class Initializer {
 private:
-    
-    //Board *board;
-    //bool changing_background;
-    //int num_gliders;
-    //std::mt19937 e2;
-
     int height_;
     int width_;
-
-
-    //void get_circle(int x, int y, int r, int* points);
-    //void get_polygon(int x, int y, int mag, int dim, float irreg, int* points);
-
-
-    //void update_board_normal();
-    //void update_board_smooth();
-    //void update_board_LtL();
-
     
 public:
     //Initializer(Board *b);
@@ -37,24 +21,6 @@ public:
     void init_center_cross(int *board, int line_width, int radius);
     void init_center_diamond(int *board, int radius);
     void init_center_square(int *board, int radius);
-    //void init_hodge_board(int n);
-    //void init_quadrants();
-    //void init_gliders();
-    //void init_circle();
-    //void init_symm();
-    //void init_smooth_life();
-    //void init_square_shell();
-    //void init_circle_shell();
-    //void init_polygon_shell();
-
-    //void init_1D_board();
-
-
-    //void make_glider(int x, int y, int orientation);
-
-    //void set_density(int new_density);
-
-    //void modify_gliders(int factor);
 
 };
 
