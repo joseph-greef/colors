5093cfeb686e0c21b4c5968e38d664d5f907a7d9
Joseph Greef <joseph.k.greef@gmail.com>
2020-12-31 23:57:55 -0800

    Move CUDA kernels to their ruleset folders

--- cuda_kernels/common.cuh
+++
@@ -1,9 +0,0 @@
-
-
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
-#define VON_NEUMANN 0
-#define MOORE 1
-
-__device__ int get_num_alive_neighbors(int x, int y, int neighborhood_type, int range, int *board, int width, int height);
